4061bd4daec8b4e8571bc40e9e1bbd4271b0c4ad
kssk3 <122779182+kssk3@users.noreply.github.com>
2023-05-22 18:49:58 +0900

    middleware1

--- src/server.js
+++ src/server.js
@@ -4,18 +4,18 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`someone is going to: ${req.url}`);
+    next();
+};
+
 const handleHome = (req, res) => {
-    return res.end();
+    return res.send("I love middleware");
 };
 
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-};
-
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", gossipMiddleware, handleHome);
+//* get에서 url에 요청를 하고 req와 res의 사이에 middleware가 응답 후 다음 함수인 handleHome을 호출함 */
 
 const handleListening = () =>
     console.log(`✅Server listenting on port http://localhost:${PORT} 🚀`);
-
 app.listen(PORT, handleListening);
