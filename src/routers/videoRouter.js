e228cd847b510800eb8da4822fa63519546d6964
kssk3 <122779182+kssk3@users.noreply.github.com>
2023-06-01 19:17:46 +0900

    pug. 6.1

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,8 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:(\\d+)", see);
+videoRouter.get("/:(\\d+)/edit", edit);
+videoRouter.get("/:(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
--- src/server.js
+++ src/server.js
@@ -6,11 +6,14 @@ import videoRouter from "./routers/videoROuter";
 
 const PORT = 4000;
 
+console.log(process.cwd());
+
 const app = express();
 const logger = morgan("dev");
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
