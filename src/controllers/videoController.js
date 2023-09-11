d1da82617990b2be5cdc8089de7f30f6c50dfb3d
kssk3 <122779182+kssk3@users.noreply.github.com>
2023-09-11 18:49:30 +0900

    #15.1 Sending Messages 9.11

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -65,6 +65,7 @@ export const postEdit = async (req, res) => {
         description,
         hashtags: Video.formatHashtags(hashtags),
     });
+    req.flash("success", "Changes saved.");
     return res.redirect(`/videos/${id}`);
 };
 
