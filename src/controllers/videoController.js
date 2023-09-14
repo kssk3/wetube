fa62d318db3202d4a58cf47c6fa3240a39d8f8be
kssk3 <122779182+kssk3@users.noreply.github.com>
2023-09-14 17:10:09 +0900

    #16.8 Comment Ids 9.14

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -155,8 +155,12 @@ export const createComment = async (req, res) => {
     if (!video) {
         return res.sendStatus(404);
     }
-    const comment = await Comment.create({ text, owner: user._id, video: id });
+    const comment = await Comment.create({
+        text,
+        owner: user._id,
+        video: id,
+    });
     video.comments.push(comment._id);
     video.save();
-    return res.sendStatus(201);
+    return res.status(201).json({ newCommentId: comment._id });
 };
